376c31e4bb9085250617e13b1914433a994c869a
Gál Péter <pepyta118@gmail.com>
2023-04-25 16:09:51 +0200

    feat: edge test

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,14 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+import { PrismaClient } from "@prisma/client"
+
+const prisma = new PrismaClient();
+
+export const config = {
+  runtime: 'edge',
+};
+
+export default async function handler(req, res) {
+  const posts = await prisma.post.findMany();
+  res.status(200).json(posts);
 }
