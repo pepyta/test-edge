d7ba28e2a0fe4077c68f815e42d5d2f5a3d559fb
Gál Péter <pepyta118@gmail.com>
2023-04-26 12:40:09 +0200

    feat: use prisma accelerate

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,8 +1,9 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 import { PrismaClient } from "@prisma/client"
+import useAccelerate from '@prisma/extension-accelerate';
 
-const prisma = new PrismaClient();
+const prisma = new PrismaClient().$extends(useAccelerate);
 
 export default async function handler(req, res) {
   const posts = await prisma.post.findMany();
